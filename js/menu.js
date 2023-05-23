0aec9ffd3ed98ad855cf5aa3f4c96a482a0d2830
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-05-23 19:41:06 +0300

    Added footer at about and change icons

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,5 @@
 const hamburgerIcon = document.querySelector(".mobile_menu");
+const body = document.querySelector("body");
 const closeIcon = document.querySelector(".menu-close");
 const menuList = document.querySelector(".menu-list");
 const menuItems = document.querySelectorAll(".menu-item");
@@ -8,12 +9,14 @@ function menuOpen() {
     menu.classList.add('open_menu');
     menuList.classList.add('open_menu');
     hamburgerIcon.classList.add('hide');
+    body.classList.add('scrollStop');
 }
 
 function menuClose() {
     menu.classList.remove('open_menu');
     menuList.classList.remove('open_menu');
     hamburgerIcon.classList.remove('hide');
+    body.classList.remove('scrollStop');
 }
 
 hamburgerIcon.addEventListener('click', menuOpen);
