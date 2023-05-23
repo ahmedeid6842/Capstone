9bdd6e1afee1750232515fab06225fcab852843f
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-05-23 20:33:27 +0300

    finsih mobile version & fix linters errors

--- js/instructor.js
+++ js/instructor.js
@@ -1,34 +1,34 @@
-const instructorsCards = document.querySelector(".instructor-main");
-const moreButton = document.querySelector(".more")
+const instructorsCards = document.querySelector('.instructor-main');
+const moreButton = document.querySelector('.more');
 const instructors = [
-    {
-        name: "Yochai Benkler",
-        bio: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-        brief: "Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006",
-        image: "./images/instructor/instructor_1.svg"
-    },
-    {
-        name: "Yochai Benkler",
-        bio: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-        brief: "Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006",
-        image: "./images/instructor/instructor_1.svg"
-    },
-    {
-        name: "Yochai Benkler",
-        bio: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-        brief: "Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006",
-        image: "./images/instructor/instructor_1.svg"
-    },
-    {
-        name: "Yochai Benkler",
-        bio: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
-        brief: "Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006",
-        image: "./images/instructor/instructor_1.svg"
-    }
-]
+  {
+    name: 'Yochai Benkler',
+    bio: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    brief: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+    image: './images/instructor/instructor_1.svg',
+  },
+  {
+    name: 'Yochai Benkler',
+    bio: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    brief: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+    image: './images/instructor/instructor_1.svg',
+  },
+  {
+    name: 'Yochai Benkler',
+    bio: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    brief: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+    image: './images/instructor/instructor_1.svg',
+  },
+  {
+    name: 'Yochai Benkler',
+    bio: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    brief: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+    image: './images/instructor/instructor_1.svg',
+  },
+];
 
 function buildInstructorCard(index) {
-    const buildCardHTML = `
+  const buildCardHTML = `
         <li class="instructor-item" >
             <img src="${instructors[index].image}" alt="${instructors[index].name}'s image" class="instructor-img">
             <div class="instructor-details">
@@ -37,24 +37,21 @@ function buildInstructorCard(index) {
                 <p>${instructors[index].brief}</p>
             </div>
         </li>
-    `
-    instructorsCards.insertAdjacentHTML('beforeend', buildCardHTML)
+    `;
+  instructorsCards.insertAdjacentHTML('beforeend', buildCardHTML);
 }
-
-
 
 let index = 0;
 function listCardInstructorMobile() {
-    for (let cardIndex = 0; cardIndex < 3; cardIndex++) {
-        buildInstructorCard(index);
-        index++;
-        if (index == instructors.length) {
-            moreButton.classList.add("hide");
-            return;
-        }
+  for (let cardIndex = 0; cardIndex < 3; cardIndex += 1) {
+    buildInstructorCard(index);
+    index += 1;
+    if (index === instructors.length) {
+      moreButton.classList.add('hide');
+      return;
     }
+  }
 }
 
 listCardInstructorMobile();
 moreButton.addEventListener('click', listCardInstructorMobile);
-
--- js/menu.js
+++ js/menu.js
@@ -1,27 +1,27 @@
-const hamburgerIcon = document.querySelector(".mobile_menu");
-const body = document.querySelector("body");
-const closeIcon = document.querySelector(".menu-close");
-const menuList = document.querySelector(".menu-list");
-const menuItems = document.querySelectorAll(".menu-item");
-const menu = document.querySelector(".menu-main");
+const hamburgerIcon = document.querySelector('.mobile_menu');
+const body = document.querySelector('body');
+const closeIcon = document.querySelector('.menu-close');
+const menuList = document.querySelector('.menu-list');
+const menuItems = document.querySelectorAll('.menu-item');
+const menu = document.querySelector('.menu-main');
 
 function menuOpen() {
-    menu.classList.add('open_menu');
-    menuList.classList.add('open_menu');
-    hamburgerIcon.classList.add('hide');
-    body.classList.add('scrollStop');
+  menu.classList.add('open_menu');
+  menuList.classList.add('open_menu');
+  hamburgerIcon.classList.add('hide');
+  body.classList.add('scrollStop');
 }
 
 function menuClose() {
-    menu.classList.remove('open_menu');
-    menuList.classList.remove('open_menu');
-    hamburgerIcon.classList.remove('hide');
-    body.classList.remove('scrollStop');
+  menu.classList.remove('open_menu');
+  menuList.classList.remove('open_menu');
+  hamburgerIcon.classList.remove('hide');
+  body.classList.remove('scrollStop');
 }
 
 hamburgerIcon.addEventListener('click', menuOpen);
 closeIcon.addEventListener('click', menuClose);
 
-for (const item of menuItems) {
-    item.addEventListener('click', menuClose);
-}
+menuItems.forEach((item) => {
+  item.addEventListener('click', menuClose);
+});
